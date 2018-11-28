66f30529ee3dc578d7ea8894a06f7f0cf8c129b3
Owen Niles <owenmniles@gmail.com>
2018-11-28 13:43:49 -0500

    More rewrites, more documentation, not all functions are tested.

--- blockchain.py
+++ blockchain.py
@@ -1,46 +1,117 @@
+from base import Block, Vote
+
+class InvalidBlockchain(Exception):
+    """Alert the user that the blocks that he has specified to not make up a
+    valid blockchain.
+    """
+
+    pass
 
 class Blockchain:
-    def __init__(self, genesis):
-        self.blocks = set([(genesis.sha256(), genesis, 0)])
+    """Store a sort of linked list of Block objects in a set.
+
+    Attributes:
+        blocks -- set of blocks that make up the blockchain
+    """
+
+    def __init__(self, blocks):
+        # Remove duplicates
+        initpool = set(blocks)
+        lastapproved = frozenset(blocks)
+
+        # Store the hashes of the last batch of approved blocks
+        self.blocks = set()
+
+        while len(initpool) > 0:
+            if len(self.blocks) == 0:
+                # Find the genesis block
+                geneses = filter(lambda b: b.prev == None, initpool)
+
+                if len(geneses) != 1 or self.add(geneses[0]) == False:
+                    # There must be exactly one genesis block
+                    raise InvalidBlockchain
+
+                lastapproved = frozenset(map(hash, geneses))
+                initpool.discard(geneses[0])
+            else:
+                candidates = frozenset(
+                    filter(lambda b: b.prev in lastapproved, initpool)
+                )
+
+                if len(candidates) < 1:
+                    # None of the other blocks fit into the chain
+                    raise InvalidBlockchain
+
+                # Attempt to add the newly-approved blocks to the blockchain
+                status = map(self.add, candidates)
+
+                if False in status:
+                    # Votes in every block must be verified
+                    raise InvalidBlockchain
+
+                lastapproved = frozenset(map(hash, candidates))
+                initpool -= candidates
 
     def add(self, block):
-        prevdepth = self.check(block)
+        """Add a block to the blockchain and return True if successful, False if
+        unsuccessful.
+        """
 
-        if prevdepth != None:
-            self.blocks.add((block.sha256(), block, prevdepth + 1))
+        if self.verify(block) or (len(self.blocks) == 0 and block.prev == None):
+            self.blocks.add(block)
             return True
         else:
             return False
 
-    def check(self, block):
-        def hashset(hashables):
-            return frozenset(map(lambda x: x.sha256(), hashables))
+    def verify(self, block):
+        """Check that a particular block can be added to the chain by
+        verifying all of the votes in the block.
+        """
 
         prevhash = block.prev
-        prevdepth = None
+        voters = frozenset(map(lambda v: v.voter, block.votes))
+
+        # Check for bad votes (e.g. double votes)
+        if prevhash == None or len(voters) != len(block.votes):
+            return False
 
         while prevhash != None:
-            try:
-                prevblock = filter(lambda b: b[0] == prevhash, self.blocks)[0]
+            # Identify the previous block in the blockchain
+            prevblock = filter(
+                lambda b: hash(b) == prevhash,
+                self.blocks
+            )[0]
+            prevvoters = frozenset(map(lambda v: v.voter, prevblock.votes))
 
-                hashes = hashset(block.data)
-                len0 = len(block.data)
-                len1 = len(hashes)
-                len2 = len(prevblock[1].data)
-                len3 = len(hashset(prevblock[1].data) - hashes)
+            # Check for bad votes
+            if prevvoters - voters != prevvoters:
+                return False
 
-                if len0 == len1 and len2 == len3:
-                    prevhash = prevblock[1].prev
+            prevhash = prevblock.prev
 
-                    if prevdepth == None:
-                        prevdepth = prevblock[2]
-                else:
-                    return None
-            except IndexError:
-                return None
+        return True
 
-        return prevdepth
+    def depth(self, block):
+        """Calculate the depth of the specified block if it exists in the
+        chain.
+        """
+
+        # The genesis block has depth 0
+        prevhash = block.prev
+        depth = 0
+
+        while prevhash != None:
+            # Find the previous block in the blockchain
+            prevblock = filter(
+                lambda b: hash(b) == prevhash,
+                self.blocks
+            )[0]
+
+            # Increment depth counter
+            prevhash = prevblock.prev
+            depth += 1
 
     def last(self):
-        return max(self.blocks, key = lambda b: b[2])
+        """Get the block at the end of the longest chain."""
 
+        return max(self.blocks, key=self.depth)
--- test.py
+++ test.py
@@ -1,87 +1,93 @@
 import unittest
 
-from blockchain import Blockchain
-from hashable import Block, Vote
+from base import Block, Vote
+from blockchain import Blockchain, InvalidBlockchain
+
+ab = {
+    'jy': Vote('Amany', 'James'),
+    'mc': Vote('Amany', 'Marcus'),
+    'rk': Vote('Amany', 'Ronny')
+}
+
+ak = {
+    'jy': Vote('Anthony', 'James'),
+    'mc': Vote('Anthony', 'Marcus'),
+    'rk': Vote('Anthony', 'Ronny')
+}
+
+on = {
+    'jy': Vote('Owen', 'James'),
+    'mc': Vote('Owen', 'Marcus'),
+    'rk': Vote('Owen', 'Ronny')
+}
+
+
 
 class TestBlock(unittest.TestCase):
+    """Test the functionality of our semi-custom hash function."""
+
     def setUp(self):
-        self.b1 = Block(
-            set([Vote('Owen', 'Marcus'), Vote('Anthony', 'Ronny')]),
-            None
-        )
-        self.b2 = Block(
-            set([Vote('Owen', 'Marcus'), Vote('Anthony', 'Ronny')]),
-            self.b1.sha256()
-        )
-        self.b3 = Block(
-            set([Vote('Owen', 'Marcus'), Vote('Anthony', 'Ronny')]),
-            self.b2.sha256()
-        )
-        self.b4 = Block(
-            set([Vote('Owen', 'Marcus'), Vote('Anthony', 'Ronny')]),
-            self.b2.sha256()
-        )
-        self.b5 = Block(
-            set([Vote('Owen', 'Marcus'), Vote('Amany', 'Ronny')]),
-            self.b2.sha256()
-        )
+        """Set up five blocks to test against one another."""
 
-    def test_sha256(self):
-        self.assertEqual(self.b1.sha256(), self.b1.sha256())
-        self.assertNotEqual(self.b1.sha256(), self.b2.sha256())
-        self.assertNotEqual(self.b2.sha256(), self.b3.sha256())
-        self.assertNotEqual(self.b3.sha256(), self.b4.sha256())
-        self.assertNotEqual(self.b3.sha256(), self.b5.sha256())
+        self.b1 = Block([on['mc'], ak['rk']], None)
+        self.b2 = Block([on['mc'], ak['rk']], hash(self.b1))
+        self.b3 = Block([on['mc'], ak['rk']], hash(self.b2))
+        self.b4 = Block([on['mc'], ak['rk']], hash(self.b2))
+        self.b5 = Block([on['mc'], ab['rk']], hash(self.b2))
 
-class TestVote(unittest.TestCase):
-    def setUp(self):
-        self.vote = Vote('Owen', 'Ronny')
-        self.hash_ = self.vote.sha256()
+    def test_hash(self):
+        """Ensure that the only time that two blocks have the same hash is when
+        the two blocks are the same block (i.e. all attributes are the same).
+        """
 
-    def test_sha256(self):
-        self.assertEqual(self.hash_, self.vote.sha256())
-        self.assertEqual(self.hash_, Vote('Owen', 'James').sha256())
-        self.assertNotEqual(self.hash_, Vote('Amany', 'Ronny').sha256())
-        self.assertNotEqual(self.hash_, Vote('Amany', 'Marcus').sha256())
+        self.assertEqual(hash(self.b1), hash(self.b1))
+        self.assertNotEqual(hash(self.b1), hash(self.b2))
+        self.assertNotEqual(hash(self.b2), hash(self.b3))
+        self.assertNotEqual(hash(self.b3), hash(self.b4))
+        self.assertNotEqual(hash(self.b3), hash(self.b5))
 
 class TestBlockchain(unittest.TestCase):
+    """Make sure that the Blockchain class behaves correctly."""
+
     def setUp(self):
-        self.genesis0 = Block(set(), None)
-        self.genesis1 = Block(set([Vote('Amany', 'James')]), None)
-        self.genesis2 = Block(
-            set([Vote('Amany', 'James'), Vote('Owen', 'Ronny')]),
-            None
-        )
-        self.chain0 = Blockchain(self.genesis0)
-        self.chain1 = Blockchain(self.genesis1)
-        self.chain2 = Blockchain(self.genesis2)
+        """Set up some blocks that can be combined in different ways to form
+        valid and invalid blockchains.
+        """
 
-    def test_add(self):
-        pass
+        self.b1 = Block([on['jy']], None)
+        self.b2 = Block([on['mc']], hash(self.b1))
+        self.b3 = Block([ab['jy']], hash(self.b1))
+        self.b4 = Block([ab['mc']], hash(self.b1))
+        self.b5 = Block([ab['rk'], ab['mc']], hash(self.b1))
+        self.b6 = Block([ak['rk']], hash(self.b3))
+        self.b7 = Block([ak['rk'], on['rk']], hash(self.b3))
 
-    def test_check(self):
-        self.assertTrue(
-            self.chain0.check(Block(set(), self.genesis0.sha256())) != None
-        )
-        self.assertTrue(
-            self.chain0.check(Block(set(), self.genesis1.sha256())) == None
-        )
-        self.assertTrue(self.chain0.check(Block(set(), None)) == None)
-        self.assertTrue(
-            self.chain0.check(Block(
-                set([Vote('Owen', 'James'), Vote('Anthony', 'James')]),
-                self.genesis0.sha256()
-            )) != None
-        )
-        self.assertTrue(
-            self.chain0.check(Block(
-                set([Vote('Owen', 'James'), Vote('Owen', 'Marcus')]),
-                self.genesis0.sha256()
-            )) == None
-        )
+    def test_init(self):
+        """Ensure that the constructor only allows users to create valid
+        blockchains.
+        """
 
-    def test_last(self):
-        pass
+        self.assertTrue(isinstance(Blockchain([self.b1]), Blockchain))
+        self.assertTrue(isinstance(Blockchain([self.b1, self.b1]), Blockchain))
+        self.assertTrue(
+            isinstance(Blockchain([self.b1, self.b3, self.b3]), Blockchain)
+        )
+        self.assertTrue(
+            isinstance(Blockchain([self.b1, self.b3, self.b6]), Blockchain)
+        )
+        self.assertTrue(isinstance(
+            Blockchain([self.b1, self.b3, self.b4, self.b6]), Blockchain
+        ))
+        with self.assertRaises(InvalidBlockchain):
+            Blockchain([self.b2])
+        with self.assertRaises(InvalidBlockchain):
+            Blockchain([self.b1, self.b6])
+        with self.assertRaises(InvalidBlockchain):
+            Blockchain([self.b1, self.b2])
+        with self.assertRaises(InvalidBlockchain):
+            Blockchain([self.b1, self.b5])
+        with self.assertRaises(InvalidBlockchain):
+            Blockchain([self.b1, self.b3, self.b4, self.b7])
 
 if __name__ == '__main__':
     unittest.main()
